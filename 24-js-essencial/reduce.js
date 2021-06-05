eb4b01f158fce587abc64407ca6dc652caaca6ad
mtmjr92 <>
2021-06-05 19:19:10 -0300

    24 - Array: Reduce #01

---
+++ 24-js-essencial/reduce.js
@@ -0,0 +1,20 @@
+const alunos = [
+  { nome: "Joao", nota: 7.3, bolsista: false },
+  { nome: "Maria", nota: 9.2, bolsista: true },
+  { nome: "Pedro", nota: 9.8, bolsista: false },
+  { nome: "Ana", nota: 8.7, bolsista: true },
+];
+
+console.log(alunos.map((a) => a.nota));
+
+const resultado = alunos
+  .map((a) => a.nota)
+  .reduce(function (acumulador, atual) {
+    console.log(acumulador, atual);
+    return acumulador + atual;
+  }, 10); // valor inicial
+
+console.log(resultado);
+
+const maior = alunos.reduce((x, y) => (x.nota > y.nota ? x : y)).nota;
+console.log(maior);
