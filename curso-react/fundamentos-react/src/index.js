3f6a05bac2139332b5e7b8f67a10e6e360eee497
mtmjr92 <>
2022-03-27 19:57:39 -0300

    componente com propreidade

--- curso-react/fundamentos-react/src/index.js
+++ curso-react/fundamentos-react/src/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReacDOM from "react-dom";
 import "./index.css"; // relative import
 import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
 
 ReacDOM.render(
   <div>
     <Primeiro></Primeiro>
+    <ComParametro
+      titulo="Situação do Aluno"
+      aluno="Pedro"
+      nota={9.3}
+    ></ComParametro>
+    <ComParametro
+      titulo="Situação do Aluno"
+      aluno="Maria"
+      nota={9.9}
+    ></ComParametro>
   </div>,
   document.getElementById("root")
 );
