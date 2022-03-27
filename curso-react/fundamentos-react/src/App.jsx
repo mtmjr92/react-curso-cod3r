be3cf0e676199bd7e3da8a1d09b4637bd05c2d08
mtmjr92 <>
2022-03-27 20:27:00 -0300

    desafio aleatorio

--- curso-react/fundamentos-react/src/App.jsx
+++ curso-react/fundamentos-react/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
+import Aleatorio from "./components/basicos/Aleatorio";
 
 export default (props) => {
   return (
     <div>
       <h2>Fundamento React 2</h2>
+      <Aleatorio min={10} max={60} />
       <Fragmento />
-
       <ComParametro
         titulo="Situação do Aluno"
         aluno="Pedro"
