1ac744672a7a3d98d1d78263e4ad844f8dd70105
mtmjr92 <>
2022-03-27 22:50:02 -0300

    componente Card #02

--- curso-react/fundamentos-react/src/App.jsx
+++ curso-react/fundamentos-react/src/App.jsx
@@ -4,31 +4,34 @@ import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Card from "./components/basicos/layout/Card";
+import "./App.css";
 
 export default (props) => {
   return (
-    <div>
-      <h1>Fundamento React 2</h1>
+    <div className="App">
+      <h1>Fundamento React</h1>
 
-      <Card titulo="#04 - Desafio Aleatório">
-        <Aleatorio min={10} max={60} />
-      </Card>
+      <div className="Cards">
+        <Card titulo="#04 - Desafio Aleatório">
+          <Aleatorio min={10} max={60} />
+        </Card>
 
-      <Card titulo="#03 - Fragmento">
-        <Fragmento />
-      </Card>
+        <Card titulo="#03 - Fragmento">
+          <Fragmento />
+        </Card>
 
-      <Card titulo="#02 - Com Parâmetro">
-        <ComParametro
-          titulo="Situação do Aluno"
-          aluno="Pedro"
-          nota={9.3}
-        ></ComParametro>
-      </Card>
+        <Card titulo="#02 - Com Parâmetro">
+          <ComParametro
+            titulo="Situação do Aluno"
+            aluno="Pedro"
+            nota={9.3}
+          ></ComParametro>
+        </Card>
 
-      <Card titulo="#01 - Fragmento">
-        <Primeiro />
-      </Card>
+        <Card titulo="#01 - Fragmento">
+          <Primeiro />
+        </Card>
+      </div>
     </div>
   );
 };
